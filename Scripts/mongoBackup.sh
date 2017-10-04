7311dffcf5da2362f494af3975a14cdcd50b7938
thetilliwilli <sergeyfiantsev@gmail.com>
2017-10-04 21:38:47 +0300

    fix backup

--- Scripts/mongoBackup.sh
+++ Scripts/mongoBackup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-was=$(cat /opt/CmsApi/current/lastUpdate.txt)
+lastUpdate=$(cat /opt/CmsApi/current/lastUpdate.txt)
+lastBackup=$(cat lastBackup.txt)
 now=$(date -u +"%Y%m%dT%H%M%SZ")
-if [[ "$now" > "$was" ]]; then (sudo mongodump --db TAG --archive=/opt/backup/mongodb/`echo $now`.archive); else echo "no"; fi
+if [[ "$lastUpdate" > "$lastBackup" ]]; then (sudo mongodump --db TAG --archive=/opt/backup/mongodb/`echo $now`.archive && echo $now > lastBackup.txt); else echo "no"; fi
