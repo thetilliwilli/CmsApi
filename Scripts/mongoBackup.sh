17b20b297703edf673cf1c02fa19ae8f9630a147
thetilliwilli <sergeyfiantsev@gmail.com>
2017-10-04 22:11:46 +0300

    fix

--- Scripts/mongoBackup.sh
+++ Scripts/mongoBackup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-lastUpdate=$(cat lastUpdate.txt)
-lastBackup=$(cat lastBackup.txt)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+lastUpdate=$(cat "$DIR"/lastUpdate.txt)
+lastBackup=$(cat "$DIR"/lastBackup.txt)
 now=$(date -u +"%Y%m%dT%H%M%SZ")
 if [[ "$lastUpdate" > "$lastBackup" ]]; then (sudo mongodump --db TAG --archive=/opt/backup/mongodb/`echo $now`.archive && sudo echo $now > lastBackup.txt); else echo "no"; fi
