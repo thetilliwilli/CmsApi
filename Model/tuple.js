aa427361a94a43c146c942d796cf0a1923f11c4f
thetilliwilli <sergeyfiantsev@gmail.com>
2017-10-08 22:05:52 +0300

    + tuple model

--- Model/tuple.js
+++ Model/tuple.js
@@ -12,17 +12,17 @@ var tupleSchema = new mongoose.Schema({
     //PROPERTIES--------------------
     guid: {type: String, default: uuid},
     name: {type: String, required: [true, "Отсутствует Название экспоната"], minlength: [1, "Слишком короткое Название экспоната"], unique: [true, "Название экспоната должно быть уникальным"]},
-    catSub: {type: String, required: [true, "Отсутствует Категория экспоната"]},
+    catsub: {type: String, required: [true, "Отсутствует Категория экспоната"]},
     countries: [String],
     description: String,
     coverImage: String,
     imageGallery: [{
-        imageSrc: String,
-        thumbnailSrc: String,
-        description: String,
+        image: String,
+        thumbnail: String,
+        description:{ru: String, en: String},
         guid: {type: String, default: uuid}
     }],
-    ttx: [{name: String, value: String}]
+    fields: [{name: String, value: String}]
 });
 
 tupleSchema.plugin(autoIncrementPlugin.plugin, "Tuple");
