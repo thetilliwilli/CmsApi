4630f06e34383c1d41d3e65dc7fa028d8713f7ee
thetilliwilli <sergeyfiantsev@gmail.com>
2017-11-02 20:28:45 +0300

    копирование изображений при клонирование Экспонатов

--- Module/repoAdapter.js
+++ Module/repoAdapter.js
@@ -9,18 +9,35 @@ module.exports = {
      * Принять на вход base64 галлерею -> сохранить на диск -> вернуть галлерею с подмененной инфой по сохранненым файлам
      */
     StoreGallery: function(galleryId, dto){
-        const gallery = dto.imageGallery.filter(img => img.image.slice(0,5) === "data:");
+        const gallery = dto.imageGallery;
         const gid = galleryId.toString();
-        const fids = [];
-        const exts = [];
-        return repo.CreateEmptyGallery(gid.toString())
-            .then(() => Promise.all(gallery.map( 
+        return Promise.resolve()
+            .then(() => repo.CreateEmptyGallery(gid.toString()))
+            .then(() => Promise.all(gallery.map(
                 (img, index) => {
-                    const info = base64ToFile(img.image);
-                    const fid = uuid();
-                    fids.push(fid);
-                    exts.push(info.ext);
-                    return repo.SaveFile(gid, fid, info.ext, info.content);
+                    if(img.image.slice(0,5) === "data:")//это base64, файл нужно пересохранить на диск
+                    {
+                        const info = base64ToFile(img.image);
+                        const fid = uuid();
+                        return repo.SaveFile(gid, fid, info.ext, info.content)
+                            .then(() => dto.imageGallery[index].image = `${config.repo.webRoot}/${gid}/${fid}.${info.ext}`);//подменить ссылку
+                    }
+                    else
+                    {
+                        if(img.image.split("/")[4] === gid.split("/")[1])//если изображение принадлежит этой галлереи то ничего не делать
+                        {
+                            return Promise.resolve();
+                        }
+                        else//изображение получено клонированием из другой галлереи, необходимо скопировать файл
+                        {
+                            const parts = img.image.split("/") ;
+                            const info = parts[5].split(".");
+                            const gidFrom = `${parts[3]}/${parts[4]}`;
+                            return repo.CopyFile(gidFrom, gid, info[0], info[1])
+                                .then(() => dto.imageGallery[index].image = `${config.repo.webRoot}/${gid}/${info[0]}.${info[1]}`);//подменить ссылку;
+                        }
+                    }
+                    
                 }
             )))
             .then(() => {//Пересохраняем coverImage
@@ -35,7 +52,6 @@ module.exports = {
                 return repo.SaveFile(gid, "cover", info.ext, info.content)
                     .then(() => dto.video = `${config.repo.webRoot}/${gid}/cover.${info.ext}`);
             })
-            .then(() => gallery.forEach( (img,i) => img.image = `${config.repo.webRoot}/${gid}/${fids[i]}.${exts[i]}`))
             .catch(error => {console.error(error); throw error;});
     },
     DeleteGallery: function(galleryId){
