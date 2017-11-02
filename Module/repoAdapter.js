d3656e600e5aef5962a1c0401fd68696b1bb9686
thetilliwilli <sergeyfiantsev@gmail.com>
2017-11-02 21:19:24 +0300

    копирование coverImage и video при клонирование Сущностей

--- Module/repoAdapter.js
+++ Module/repoAdapter.js
@@ -41,16 +41,52 @@ module.exports = {
                 }
             )))
             .then(() => {//Пересохраняем coverImage
-                if(!dto.coverImage || dto.coverImage.slice(0,5) !== "data:") return;//Выходим если не base64 изображение
-                const info = base64ToFile(dto.coverImage);
-                return repo.SaveFile(gid, "cover", info.ext, info.content)
-                    .then(() => dto.coverImage = `${config.repo.webRoot}/${gid}/cover.${info.ext}`);
+                if(!dto.coverImage) return true;
+                if(dto.coverImage.slice(0,5) === "data:")
+                {
+                    const info = base64ToFile(dto.coverImage);
+                    return repo.SaveFile(gid, "cover", info.ext, info.content)
+                        .then(() => dto.coverImage = `${config.repo.webRoot}/${gid}/cover.${info.ext}`);
+                }
+                else
+                {
+                    if(dto.coverImage.split("/")[4] === gid.split("/")[1])//если изображение принадлежит этой галлереи то ничего не делать
+                    {
+                        return Promise.resolve();
+                    }
+                    else
+                    {
+                        const parts = dto.coverImage.split("/");
+                        const info = parts[5].split(".");
+                        const gidFrom = `${parts[3]}/${parts[4]}`;
+                        return repo.CopyFile(gidFrom, gid, info[0], info[1])
+                            .then(() => dto.coverImage = `${config.repo.webRoot}/${gid}/cover.${info[1]}`);//подменить ссылку;
+                    }
+                }
             })
             .then(() => {//Пересохраняем video (Golo)
-                if(!dto.video || dto.video.slice(0,5) !== "data:") return;//Выходим если не base64
-                const info = base64ToFile(dto.video);
-                return repo.SaveFile(gid, "cover", info.ext, info.content)
-                    .then(() => dto.video = `${config.repo.webRoot}/${gid}/cover.${info.ext}`);
+                if(!dto.video) return;
+                if(dto.video.slice(0,5) === "data:")
+                {
+                    const info = base64ToFile(dto.video);
+                    return repo.SaveFile(gid, "cover", info.ext, info.content)
+                        .then(() => dto.video = `${config.repo.webRoot}/${gid}/cover.${info.ext}`);
+                }
+                else
+                {
+                    if(dto.video.split("/")[4] === gid.split("/")[1])//если изображение принадлежит этой галлереи то ничего не делать
+                    {
+                        return Promise.resolve();
+                    }
+                    else
+                    {
+                        const parts = dto.video.split("/");
+                        const info = parts[5].split(".");
+                        const gidFrom = `${parts[3]}/${parts[4]}`;
+                        return repo.CopyFile(gidFrom, gid, info[0], info[1])
+                            .then(() => dto.video = `${config.repo.webRoot}/${gid}/cover.${info[1]}`);//подменить ссылку;
+                    }
+                }
             })
             .catch(error => {console.error(error); throw error;});
     },
